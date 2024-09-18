3be9cfb30cacd8e77d766fce2102f25dc0389e56
Tapertii <113117498+cygnn@users.noreply.github.com>
2024-09-18 16:22:06 +0800

    Add files via upload

---
+++ SIMPLETRON/memory.py
@@ -0,0 +1,9 @@
+'''
+    DURANO, Dennis S. BSCPE4
+	Simpleton Memory
+'''
+class Memory(object):
+	def __init__(self):pass #class constructor
+	def store_data(self,address:int,data:str)->bool: pass #method
+	def read_data(self,address:int,data:str)->str:pass
+	def dump(self)->bool: pass
